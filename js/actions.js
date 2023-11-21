f5b31969c3af4aeb3867340387a5f736a5ea6808
4m12-code <112677210+4m12-code@users.noreply.github.com>
2023-11-20 21:54:15 -0500

    se corrigió que el frontend aputara a la api desde un archivo php llamado autent.php, alli se establece los datos del formulario a postear y se compara la respuesta para seguir al login o inicio del programa

--- js/actions.js
+++ js/actions.js
@@ -9,3 +9,29 @@ jQuery(function($) {
     mousedrag: true,  // Habilitar el arrastre
   });
 });
+/*
+//Escoger el form por su id
+const fromElement =  document.getElementById("formu");
+fromElement.addEventListener("submit",(event) => {
+  
+  event.preventDefault();
+  let user = document.getElementById("user").value;
+  let pass = document.getElementById("pass").value;
+  let transaction = {user:user, pass:pass}
+  let transactionJson = JSON.stringify(transaction);
+ 
+  const options = {
+    method : "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body : transactionJson
+  }
+  fetch('http://localhost:30001/api/user',options)
+    .then(res => res.json())
+    .then(response =>{
+      console.log(response);
+      (response == "correcto") ? alert("Iniciaste Sesión"):alert("Usuario o contraseña Incorrectos");
+    })
+})
+*/
